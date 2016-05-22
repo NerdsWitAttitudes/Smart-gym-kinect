3950cd0c12915de215666177e7fdf4c81ea22567
drwitter <davey.r.witter@gmail.com>
2016-05-22 18:56:12 +0200

    ColorFrameReady

--- KinectSaveModel/MainWindow.xaml.cs
+++ KinectSaveModel/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
@@ -7,41 +8,25 @@ using System.Windows.Shapes;
 using Microsoft.Kinect;
 using System.Linq;
 using System.IO;
-using System.Windows.Media;
 using System.Windows.Media.Media3D;
 using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace KinectSaveModel
 {
     public partial class MainWindow : Window
     {
+        private WriteableBitmap colorBitmap;
+        private byte[] colorPixels;
         KinectSensor sensor = KinectSensor.KinectSensors[0];
         byte[] pixelData;
         Skeleton[] skeleton;
+        int frameNumber = 0;
+        private string path = @"C:\KinectSavedMovements\FirstMovement.txt";
 
         public MainWindow()
         {
             InitializeComponent();
-
-            // Test data benodigd voor het schrijven naar bestand te testen
-            /*Vector3D ShoulderCenter = new Vector3D(10,10,30);
-            Vector3D RightShoulder = new Vector3D(11,11,31);
-            Vector3D LeftShoulder = new Vector3D(12,12,32);
-            Vector3D RightElbow = new Vector3D(13,13,33);
-            Vector3D LeftElbow = new Vector3D(14,14,34);
-            Vector3D RightWrist = new Vector3D(15,15,35);
-            Vector3D LeftWrist = new Vector3D(16,16,36);
-
-            List<Vector3D> vectors = new List<Vector3D>();
-            vectors.Add(ShoulderCenter);
-            vectors.Add(RightShoulder);
-            vectors.Add(LeftShoulder);
-            vectors.Add(RightElbow);
-            vectors.Add(LeftElbow);
-            vectors.Add(RightWrist);
-            vectors.Add(LeftWrist);
-
-            writeToFile(vectors);*/
 
             this.Loaded += new RoutedEventHandler(Window_Loaded);
             this.Unloaded += new RoutedEventHandler(Window_Closed);
@@ -56,7 +41,7 @@ namespace KinectSaveModel
             {
                 if (SkeletonFrame == null)
                 {
-
+                    Debug.WriteLine("frame == null");
                     // Moet nog een foutmelding voor aangemaakt worden
                 }
                 else
@@ -75,26 +60,16 @@ namespace KinectSaveModel
                                             select s).FirstOrDefault();
                 if (currentSkeleton != null)
                 {
-                    
-                    Vector3D ShoulderCenter = new Vector3D(currentSkeleton.Joints[JointType.ShoulderCenter].Position.X, currentSkeleton.Joints[JointType.ShoulderCenter].Position.Y, currentSkeleton.Joints[JointType.ShoulderCenter].Position.Z);
-                    Vector3D RightShoulder = new Vector3D(currentSkeleton.Joints[JointType.ShoulderRight].Position.X, currentSkeleton.Joints[JointType.ShoulderRight].Position.Y, currentSkeleton.Joints[JointType.ShoulderRight].Position.Z);
-                    Vector3D LeftShoulder = new Vector3D(currentSkeleton.Joints[JointType.ShoulderLeft].Position.X, currentSkeleton.Joints[JointType.ShoulderLeft].Position.Y, currentSkeleton.Joints[JointType.ShoulderLeft].Position.Z);
-                    Vector3D RightElbow = new Vector3D(currentSkeleton.Joints[JointType.ElbowRight].Position.X, currentSkeleton.Joints[JointType.ElbowRight].Position.Y, currentSkeleton.Joints[JointType.ElbowRight].Position.Z);
-                    Vector3D LeftElbow = new Vector3D(currentSkeleton.Joints[JointType.ElbowLeft].Position.X, currentSkeleton.Joints[JointType.ElbowLeft].Position.Y, currentSkeleton.Joints[JointType.ElbowLeft].Position.Z);
-                    Vector3D RightWrist = new Vector3D(currentSkeleton.Joints[JointType.WristRight].Position.X, currentSkeleton.Joints[JointType.WristRight].Position.Y, currentSkeleton.Joints[JointType.WristRight].Position.Z);
-                    Vector3D LeftWrist = new Vector3D(currentSkeleton.Joints[JointType.WristLeft].Position.X, currentSkeleton.Joints[JointType.WristLeft].Position.Y, currentSkeleton.Joints[JointType.WristLeft].Position.Z);
-
                     List<Vector3D> vectors = new List<Vector3D>();
-                    vectors.Add(ShoulderCenter);
-                    vectors.Add(RightShoulder);
-                    vectors.Add(LeftShoulder);
-                    vectors.Add(RightElbow);
-                    vectors.Add(LeftElbow);
-                    vectors.Add(RightWrist);
-                    vectors.Add(LeftWrist);
-
+                    vectors.Add(new Vector3D(currentSkeleton.Joints[JointType.ShoulderCenter].Position.X, currentSkeleton.Joints[JointType.ShoulderCenter].Position.Y, currentSkeleton.Joints[JointType.ShoulderCenter].Position.Z));
+                    vectors.Add(new Vector3D(currentSkeleton.Joints[JointType.ShoulderRight].Position.X, currentSkeleton.Joints[JointType.ShoulderRight].Position.Y, currentSkeleton.Joints[JointType.ShoulderRight].Position.Z));
+                    vectors.Add(new Vector3D(currentSkeleton.Joints[JointType.ShoulderLeft].Position.X, currentSkeleton.Joints[JointType.ShoulderLeft].Position.Y, currentSkeleton.Joints[JointType.ShoulderLeft].Position.Z));
+                    vectors.Add(new Vector3D(currentSkeleton.Joints[JointType.ElbowRight].Position.X, currentSkeleton.Joints[JointType.ElbowRight].Position.Y, currentSkeleton.Joints[JointType.ElbowRight].Position.Z));
+                    vectors.Add(new Vector3D(currentSkeleton.Joints[JointType.ElbowLeft].Position.X, currentSkeleton.Joints[JointType.ElbowLeft].Position.Y, currentSkeleton.Joints[JointType.ElbowLeft].Position.Z));
+                    vectors.Add(new Vector3D(currentSkeleton.Joints[JointType.WristRight].Position.X, currentSkeleton.Joints[JointType.WristRight].Position.Y, currentSkeleton.Joints[JointType.WristRight].Position.Z));
+                    vectors.Add(new Vector3D(currentSkeleton.Joints[JointType.WristLeft].Position.X, currentSkeleton.Joints[JointType.WristLeft].Position.Y, currentSkeleton.Joints[JointType.WristLeft].Position.Z));
+                    
                     writeToFile(vectors);
-
                 }
             }
         }
@@ -103,31 +78,39 @@ namespace KinectSaveModel
         {
             string line = "";
             String timeStamp = GetTimestamp(DateTime.Now);
-            line += timeStamp + "@";
+            String frame = ""+frameNumber;
+            line += frame+",["+timeStamp + "]@";
             for (int i = 0; i < vectorList.Count; i++)
             {
-                line += "["+vectorList[i]+"]";
+                if (i != (vectorList.Count-1))
+                {
+                    line += "[" + vectorList[i] + "],";
+                }
+                else
+                {
+                    line += "[" + vectorList[i] + "]";
+                }
             }
-            string path = @"C:\KinectSavedMovements\FirstMovement.txt";
             if (!File.Exists(path))
             {
                 File.WriteAllText(path, line);
             }
                 else
             {
+                if (frameNumber == 0)
+                {
+                    String date = GetTimestamp(DateTime.Now);
+                    date = date.Replace("/", "-");
+                    date = date.Replace(" ", "_");
+                    date = date.Replace(":", "-");
+                    path = @"C:\KinectSavedMovements\FirstMovement_" + date + ".txt";
+                }
                 File.AppendAllText(path, Environment.NewLine + line);
-                
-
-                // Deze moet alleen bij de eerste frame gebruikt worden. Op deze manier overscrhijft hij geen bestaande bestanden
-                /*String date = GetTimestamp(DateTime.Now);
-                date = date.Replace("/","-");
-                date = date.Replace(" ","_");
-                date = date.Replace(":","-");
-                path = @"C:\KinectSavedMovements\FirstMovement_"+ date + ".txt";*/
             }
+            frameNumber = frameNumber + 1;
         }
 
-        public static String GetTimestamp(DateTime value)
+        public String GetTimestamp(DateTime value)
         {
             return value.ToString("yyyy:MM:dd HH/mm/ss/ffff");
         }
@@ -141,8 +124,29 @@ namespace KinectSaveModel
         // Deze methode wordt aangeroepen wanneer het scherm is geladen
         private void Window_Loaded(object sender, EventArgs e)
         {
+            sensor.ColorStream.Enable(ColorImageFormat.RgbResolution640x480Fps30);
+            colorPixels = new byte[sensor.ColorStream.FramePixelDataLength];
+            colorBitmap = new WriteableBitmap(sensor.ColorStream.FrameWidth, this.sensor.ColorStream.FrameHeight, 96.0, 96.0, PixelFormats.Bgr32, null);
+            Image.Source = this.colorBitmap;
+            this.sensor.ColorFrameReady += this.SensorColorFrameReady;
             sensor.SkeletonFrameReady += skeletonFrameReady;
             sensor.Start();
+        }
+
+        private void SensorColorFrameReady(object sender, ColorImageFrameReadyEventArgs e)
+        {
+            using (ColorImageFrame colorFrame = e.OpenColorImageFrame())
+            {
+                if (colorFrame != null)
+                {
+                    colorFrame.CopyPixelDataTo(this.colorPixels);
+                    this.colorBitmap.WritePixels(
+                        new Int32Rect(0, 0, this.colorBitmap.PixelWidth, this.colorBitmap.PixelHeight),
+                        this.colorPixels,
+                        this.colorBitmap.PixelWidth * sizeof(int),
+                        0);
+                }
+            }
         }
     }
 }
